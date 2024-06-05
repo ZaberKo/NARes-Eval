bc0c90031acb3430b56d422cf22a469c8f231b5f
zaber <20830726+ZaberKo@users.noreply.github.com>
2024-06-05 23:01:18 +0800

    update run.sh with seed support

--- run.sh
+++ run.sh
@@ -4,20 +4,21 @@ set -eu
 GPU=${1:-0}
 arch_list=${2:-arch_list.txt}
 attack_type=${3:-base}
-norm=${4:-Linf}
+seed=${4:-0}
+norm=${5:-Linf}
 
 
 while IFS= read -r arch_id; do
     model=arch_$arch_id
     if [[ ${attack_type} == "base" ]]
     then
-        (set +x; CUDA_VISIBLE_DEVICES=${GPU} python ./eval.py --model-path ./models_home --log-path ./attack_log --load-best-model --model ${model} --seed 0 --norm ${norm})
+        (set +x; CUDA_VISIBLE_DEVICES=${GPU} python ./eval.py --model-path ./models_home --log-path ./attack_log --load-best-model --model ${model} --seed ${seed} --norm ${norm})
     elif [[ ${attack_type} == "aa" ]]
     then
-        (set +x; CUDA_VISIBLE_DEVICES=${GPU} python ./eval.py --model-path ./models_home --log-path ./attack_log --load-best-model --model ${model} --seed 0  --norm ${norm} --attack-choice AA --aa-type Compact)
+        (set +x; CUDA_VISIBLE_DEVICES=${GPU} python ./eval.py --model-path ./models_home --log-path ./attack_log --load-best-model --model ${model} --seed ${seed} --norm ${norm} --attack-choice AA --aa-type Compact)
     elif [[ ${attack_type} == "corruption" ]]
     then
-        (set +x; CUDA_VISIBLE_DEVICES=${GPU} python ./eval_corruption_level.py --model-path ./models_home --log-path ./attack_log --load-best-model --model ${model} --seed 0)
+        (set +x; CUDA_VISIBLE_DEVICES=${GPU} python ./eval_corruption_level.py --model-path ./models_home --log-path ./attack_log --load-best-model --model ${model} --seed ${seed})
     else
         echo "Invalid attack type: ${attack_type}"
     fi
